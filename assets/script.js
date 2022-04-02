f012f76b34b8637200c0c24579d99c545c78a0be
LA WALKER <lauraashleywalker4@gmail.com>
2022-04-02 14:22:33 -0400

    html update

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+//Current day and time
+document.querySelector('#currentDay').textContent = moment().format('dddd') + "" + moment().format("YYYY MM DD");
+
+//Hour by hour
+var hour9am = moment().hour(9am);
+var hour10am = moment().hour(10am);
+var hour11am = moment().hour(11am);
+var hour12pm = moment().hour(12pm);
+var hour13pm = moment().hour(1pm);
+var hour14pm = moment().hour(2pm);
+var hour15pm = moment().hour(3pm);
+var hour16pm = moment().hour(4pm);
+
+var hour = moment().format
+var minute = monment().format('m');
+

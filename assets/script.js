829621413b0225791bd14529a60d5a0d5a56fcbd
LA WALKER <lauraashleywalker4@gmail.com>
2022-04-02 15:28:46 -0400

    update

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,30 @@
 //Current day and time
-document.querySelector('#currentDay').textContent = moment().format('dddd') + "" + moment().format("YYYY MM DD");
+const d = new Date("MM,DD,YYYY");
+document.getElementById("demo").innerHTML = d;
+
+var currentDate = moment().format('dddd') + "" + momet().format("YYYY, MM, DD");
+var currentHour = moment().format('h:mm:ss a');
 
 //Hour by hour
-var hour9am = moment().hour(9am);
-var hour10am = moment().hour(10am);
-var hour11am = moment().hour(11am);
-var hour12pm = moment().hour(12pm);
-var hour13pm = moment().hour(1pm);
-var hour14pm = moment().hour(2pm);
-var hour15pm = moment().hour(3pm);
-var hour16pm = moment().hour(4pm);
+var inputhour9am = moment().hour("9am");
+var inputhour10am = moment().hour("10am");
+var inputhour11am = moment().hour("11am");
+var inputhour12pm = moment().hour("12pm");
+var inputhour1pm = moment().hour("1pm");
+var inputhour2pm = moment().hour("2pm");
+var inputhour3pm = moment().hour("3pm");
+var inputhour4pm = moment().hour("4pm");
 
-var hour = moment().format
-var minute = monment().format('m');
+var Input;
+var hourSpan;
+
+
+
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
 
 
 
